0ad8c15d1c3c5f8f09c80370b72300271d13b149
andrei_voitau <a_voitau@indatalabs.com>
2020-04-14 16:14:54 +0300

    first commit

---
+++ config/keys.js
@@ -0,0 +1,12 @@
+module.exports = {
+    google: {
+        clientID: '788409067155-ltbkhigigci5dukmbr1058n1kof93ohc.apps.googleusercontent.com',
+        clientSecret: 'frzOc4tnhb5yBe6yF6V0zQaQ'
+    },
+    mongodb: {
+        dbURI: 'mongodb://localhost:27017/google-oauth-test1'
+    },
+    session: {
+        cookieKey: 'secret'
+    }
+};
---
+++ routes/auth.routes.js
@@ -0,0 +1,23 @@
+const router = require('express').Router();
+const passport = require('passport');
+
+//auth login
+router.get('/login', (req, res) => {
+   res.render('login', {user: req.user});
+});
+
+router.get('/logout', (req, res) => {
+    req.logout();
+    res.redirect('/');
+});
+
+router.get('/google', passport.authenticate('google', {
+    scope: ['profile']
+}));
+
+//callback route for google to redirect to
+router.get('/google/redirect', passport.authenticate('google'), (req, res) => {
+    res.redirect('/profile/');
+});
+
+module.exports = router;
